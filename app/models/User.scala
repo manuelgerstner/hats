acf8fe26cf11f3edba3dfd13e81e0cfb5fcde3ee
NemoOudeis <fuguido@googlemail.com>
2013-11-28 13:51:25 +0100

    feature: #4 preliminary Data Model completed
    Created and refined models for Card, Thinking-
    Session, Hat and User with corresponding SQL schema.
    All Model case classes have an object (= scala for
    singleton) that offers convenience crud like operations
    (where they make sense).
    The utility methods are very object oriented atm,
    we can change this in no time if it's better for
    the frontend/app logic.
    All models and methods are commented.
    All model modifications should be done in a new
    evolution (i.e. 2.sql, 3.sql etc.).

---
+++ app/models/User.scala
@@ -0,0 +1,75 @@
+package models
+
+import play.api.db.DB
+import play.api.db._
+
+import play.api.Play.current
+import anorm._
+import anorm.SqlParser._
+
+/**
+ * Models participants and moderators/creators of ThinkingSession. No authentication done by us, so
+ * we keep the data to a minimum.
+ * Possible Additions: eMail, Image/ImageUrl, last (browser) session, list of ThinkingSessions (access)
+ *
+ * We do not need the Users for the this Monday (2nd Dec)
+ *
+ * @author Nemo
+ */
+case class User(id: Long, name: String)
+
+object User {
+
+  /**
+   * ORM simple
+   */
+  def simple = {
+    get[Long]("id") ~
+      get[String]("name") map {
+        case id ~ name => User(id, name)
+      }
+  }
+
+  /**
+   * Retrieve all users in the system
+   */
+  def all(): List[User] = {
+    DB.withConnection { implicit connection =>
+      SQL("select * from user").as(User.simple *)
+    }
+  }
+
+  /**
+   * Find a user by id
+   */
+  def getById(id: Long): User = {
+    DB.withConnection { implicit connection =>
+      SQL("select * from user where id = {id}").on(
+        'id -> id
+      ).as(User.simple *) head
+    }
+  }
+
+  /**
+   * Create a new user by a name.
+   * This will NOT return the created User!
+   */
+  def create(name: String) = {
+    DB.withConnection { implicit connection =>
+      SQL("insert into user (name) values ({name})").on(
+        'name -> name
+      ).executeUpdate()
+    }
+  }
+
+  /**
+   * Delete a user
+   */
+  def delete(user: User) = {
+    DB.withConnection { implicit connection =>
+      SQL("delete from user where id = {id}").on(
+        'id -> user.id
+      ).executeUpdate()
+    }
+  }
+}
