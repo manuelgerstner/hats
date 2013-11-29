742751615d3a4cb4aff783229c5c08ddc3ec6d00
NemoOudeis <fuguido@googlemail.com>
2013-11-30 00:07:35 +0100

    feature: #22 refined the URL patterns a little bit
    changed Card and ThinkingSession controller names 
    to Cards and ThinkingSessions (to avoid confusion 
    with the model) and altered the controller function
    interfaces a little bit as a result of the changes
    in the model classes

--- app/controllers/Cards.scala
+++ app/controllers/Cards.scala
@@ -10,17 +10,25 @@ import play.api.data.Forms._
  *
  * @author: NemoOudeis
  */
-object Card extends Controller {
+object Cards extends Controller {
 
   /**
-   * TODO: read card value from params, create new card in db and redirect to session index
+   * TODO: read card value from params.
+   * Needed Info from HTML Params:
+   * - HatID : Long
+   * - UserID : Long
+   * - content : String
+   * For Hat and User the db ids are expected.
+   * Content needs to be a string obviously.
    */
-  def newCard(id: Long) = TODO
+  def addCard(thinkingSessionId: Long) = TODO
 
   /**
    * TODO: retrieve card specified by session id and card id from db and update id. Redirect to session index
+   * Needed Info from HTML Params:
+   * - newContent: String
    */
-  def editCard(id: Long, cardId: Long) = TODO
+  def editCard(sessionId: Long, cardId: Long) = TODO
 
   /**
    * TODO: delete Card specified by the ids, redirect to session index
