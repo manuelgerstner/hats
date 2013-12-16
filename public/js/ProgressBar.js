39cf981992573334900b18f5467afd67191b1760
Dom <dombusser@gmail.com>
2013-12-16 12:30:35 +0100

    js: ProgressBar class and minor styling fixes

---
+++ public/js/ProgressBar.js
@@ -0,0 +1,52 @@
+/**
+ *  ProgressBar class
+ *  @author dbusser
+ */
+
+/**
+ * Constructor
+ * 
+ * @param container - a selector which holds the progress bar (should be type svg)
+ * @bubbles (optional) - initial bubbles to be set up with custom counts
+ */
+function ProgressBar(container, bubbles) {
+	this.factor = 10;
+	this.container = container;
+	this.bubbles = bubbles || {
+		// todo: use maps in the future to hold actual bubbles
+		"blue": 0,
+		"white": 0,
+		"red": 0,
+		"yellow": 0,
+		"green": 0,
+		"black": 0
+	};
+}
+/**
+ * adds a card to the progress bar inside the corresponding hat
+ * 
+ * @param card - a Card object that should be added to the progress bar
+ */
+ProgressBar.prototype.add = function(card) {
+	//console.log("adding card: ", card);
+	this.bubbles[card.hat]++;
+	this.render();
+};
+
+/**
+ * refreshes the circles in the progress bar to hold corresponding sizes
+ * 
+ */
+ProgressBar.prototype.render = function() {
+	var self = this;
+	for (var hat in self.bubbles) {
+		var count = self.bubbles[hat];
+		//console.log("hat: %s", hat);
+		//console.log("count: %d", count);
+		if (count > 0) {
+			var circle = $(self.container).find('circle.' + hat);
+			//console.log("current size %d", circle.attr("r"));
+			circle.attr('r', count * self.factor);
+		}
+	}
+};
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,3 @@
-// Contains progress outline data
-var PROGRESS_DATA = [];
-
 // Scrolls the view down to the config pane on the frontpage
 $.fn.scrollView = function() {
 	return this.each(function() {
@@ -10,75 +7,11 @@ $.fn.scrollView = function() {
 	});
 };
 
-// Adds a bubble to the global bubble string array
-function addBubbleToProgressBar(color, size, tooltip, timestamp) {
-	PROGRESS_DATA.push({ "color" : color, "size" : size });
-}
-
-// Renders the progressbar given an array containing bubble data
-function renderProgressOutline() {
-	// Sample array
-	var sampleProgress = [ { "color" : "white", "size" : 16 },
-	                       { "color" : "red", "size" : 16 },
-	                       { "color" : "yellow", "size" : 16 },
-	                       { "color" : "black", "size" : 16 },
-	                       { "color" : "green", "size" : 16 },
-	                       { "color" : "blue", "size" : 16 }];
-
-	var colorCode;
-
-	// Maximum number of bubbles to show
-	var maxBubbles = 16;
-	// Width of the progress bar in pixels
-	var barWidth = 800;
-	// Space available to each bubble in pixels
-	var bubbleSpace = barWidth / maxBubbles;
-
-	// Current position on the progress bar
-	var currentPosition = 25;
-
-	for (var i=0;i<PROGRESS_DATA.length;i++){
-        var bubble = PROGRESS_DATA[i];
-        // Get color code for color, default green is too dark
-        // @manu: why don'y you use a map here? wouldn't that be more concise? - david
-        switch (bubble.color) {
-		  case "white":
-		    colorCode = "#ffffff";
-		    break;
-		  case "red":
-		    colorCode = "#ff0000";
-		    break;
-		  case "green":
-		    colorCode = "#00cc00";
-		    break;
-		  case "yellow":
-		    colorCode = "#ffff00";
-		    break;
-		  case "blue":
-		  	colorCode = "#0000ff";
-		  	break;
-		  default:
-		    colorCode = "#000000";
-		    break;
-		}
-        var circle = $('<circle/>');
-        circle.addClass('process_bubble', bubble.color, 'tooltipster');
-        circle.attr('cx', currentPosition);
-        circle.attr('cy', 50);
-        circle.attr('r', bubble.size);
-        circle.css('fill', colorCode);
-        $(circle).appendTo('#progressOutline');
-        
-        
-		//$('#progressOutline').append('<circle class="process_bubble ' + bubble.color + ' tooltipster" cx="' + currentPosition + '" cy="50" r="' + bubble.size + '" style="fill:' + colorCode + ' ;"/>');
-        currentPosition += bubbleSpace;
-        // "Hack" for svg to reload after using jquerys append method
-        $("#svg").html($("#svg").html());
-    }
-}
 
 $(function() {
 
+	var progressBar = new ProgressBar('#progressBar');
+	
 	// initialize tooltips
 	$('.tooltipster').tooltipster();
 
@@ -86,6 +19,7 @@ $(function() {
 	if (typeof CARDS !== "undefined" && CARDS.length > 0) {
 		$(CARDS).each(function() {
 			addCard(this);
+			progressBar.add(this);
 		});
 	}
 
@@ -93,12 +27,13 @@ $(function() {
 		dataType: "json",
 		type: "post",
 		//beforeSubmit: showRequest,
-		success: function(data) {
-			if (data.error === true) {
-				alert(data.message);
+		success: function(card) {
+			if (card.error === true) {
+				alert(card.message);
 				return;
 			}
-			addCard(data); // post-submit callback
+			addCard(card); // post-submit callback
+			progressBar.add(card);
 		}
 	});
 
@@ -134,7 +69,7 @@ $(function() {
 function moveTo(hat) {
 	// change hat class of div
 	$('#hat').removeClass(HAT).addClass(hat.toLowerCase());
-	$('#body').removeClass(HAT).addClass(hat.toLowerCase());
+	$('body').removeClass(HAT).addClass(hat.toLowerCase());
 	$('#form-hat').val(hat.toLowerCase());
 	// overwrite var
 	HAT = hat;
@@ -154,6 +89,6 @@ function addCard(card, effect) {
 	//if (effect) markup.effect('highlight', {}, 1000);
 	
 	// reset card content field
-	$('#content').val('');
+	$('#content').val("");
 	$('#nocardsyet').remove();
 }
