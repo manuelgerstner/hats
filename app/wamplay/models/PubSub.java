de5d8ba628d14d0c4f09f72ef1e63b450ca79828
Dom <dombusser@gmail.com>
2014-01-15 10:20:41 +0100

    fixed indentation across files

--- app/wamplay/models/PubSub.java
+++ app/wamplay/models/PubSub.java
@@ -5,8 +5,6 @@ import java.lang.reflect.Method;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
-import com.fasterxml.jackson.databind.JsonNode;
-
 import play.Logger;
 import play.Logger.ALogger;
 import wamplay.annotations.onPublish;
@@ -16,27 +14,34 @@ import wamplay.callbacks.PubSubCallback;
 import wamplay.callbacks.SubCallback;
 import wamplay.controllers.WAMPlayContoller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 
 public class PubSub {
 	static ALogger log = Logger.of(PubSub.class.getSimpleName());
 	static ConcurrentMap<String, PubSubCallback> topics = new ConcurrentHashMap<String, PubSubCallback>();
 
-	public static void addController(String prefix, final WAMPlayContoller controller) {
+	public static void addController(String prefix,
+			final WAMPlayContoller controller) {
 
 		for (final Method method : controller.getClass().getMethods()) {
 			if (method.isAnnotationPresent(onPublish.class)) {
-				String topic = prefix + method.getAnnotation(onPublish.class).value();
+				String topic = prefix
+						+ method.getAnnotation(onPublish.class).value();
 				PubCallback cb = new PubCallback() {
 
 					@Override
-					protected JsonNode onPublish(String sessionID, JsonNode eventJson) {
+					protected JsonNode onPublish(String sessionID,
+							JsonNode eventJson) {
 						try {
-							return (JsonNode) method.invoke(controller, sessionID, eventJson);
+							return (JsonNode) method.invoke(controller,
+									sessionID, eventJson);
 						} catch (IllegalAccessException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						} catch (IllegalArgumentException e) {
-							log.error(controller.getClass().getSimpleName() + " " + method.getName() + " has incorrect arguments!");
+							log.error(controller.getClass().getSimpleName()
+									+ " " + method.getName()
+									+ " has incorrect arguments!");
 							e.printStackTrace();
 						} catch (InvocationTargetException e) {
 							// TODO Auto-generated catch block
@@ -49,18 +54,22 @@ public class PubSub {
 			}
 
 			if (method.isAnnotationPresent(onSubscribe.class)) {
-				String topic = prefix + method.getAnnotation(onSubscribe.class).value();
+				String topic = prefix
+						+ method.getAnnotation(onSubscribe.class).value();
 				SubCallback cb = new SubCallback() {
 
 					@Override
 					protected boolean onSubscribe(String sessionID) {
 						try {
-							return (boolean) method.invoke(controller, sessionID);
+							return (Boolean) method.invoke(controller,
+									sessionID);
 						} catch (IllegalAccessException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						} catch (IllegalArgumentException e) {
-							log.error(controller.getClass().getSimpleName() + " " + method.getName() + " has incorrect arguments!");
+							log.error(controller.getClass().getSimpleName()
+									+ " " + method.getName()
+									+ " has incorrect arguments!");
 							e.printStackTrace();
 						} catch (InvocationTargetException e) {
 							// TODO Auto-generated catch block
@@ -85,7 +94,7 @@ public class PubSub {
 	private static PubSubCallback createOrGet(String topic) {
 		PubSubCallback pub = topics.get(topic);
 		if (pub == null) {
-			pub =  new PubSubCallback();
+			pub = new PubSubCallback();
 			topics.put(topic, pub);
 		}
 		return pub;
