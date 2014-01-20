3f05babf29966c1645924ec4c39e8eb22e19e46a
NemoOudeis <fuguido@googlemail.com>
2014-01-20 16:24:05 +0900

    feature#73 send Mails
    adds mail templates

--- app/controllers/ThinkingSessions.scala
+++ app/controllers/ThinkingSessions.scala
@@ -100,24 +100,23 @@ object ThinkingSessions extends Controller {
     request.cookies.get(User.idCookie) match {
       case Some(cookie) =>
 
-        def sendMails(mails: List[String], title: String) {
+        def sendMails(mails: List[String], title: String, url: String) {
           mails match {
             case m :: ms =>
-              val body = new Body(views.txt.email.invite.render().toString(),
-                views.html.email.invite.render().toString());
-              val test = m
+              val body = new Body(views.txt.email.invite.render(title, url).toString(),
+                views.html.email.invite.render(title, url).toString());
               Mailer.getDefaultMailer().sendMail("Invite to Thinking Session", body, m);
-              Logger.debug("Invited User " + m + "to thinking session " + title)
-              sendMails(ms, title)
+              Logger.debug("Invited User " + m + " to thinking session " + title)
+              sendMails(ms, title, url)
             case Nil =>
-              Logger.debug("All Users invited to session " + title)
+              Logger.info("All Users invited to session " + title)
           }
         }
 
         // TODO persist hatFlow
         val user = User.byCookie(cookie).get;
         val newSessionId = ThinkingSession.create(user, form.topic, Hat.dummy)
-        sendMails(form.mailAddressList, form.topic)
+        sendMails(form.mailAddressList, form.topic, routes.ThinkingSessions.index(newSessionId).absoluteURL(false))
 
         WAMPlayServer.addTopic("thinkingSession_" + newSessionId)
         Logger.debug("Found user cookie, creating session " + newSessionId)
