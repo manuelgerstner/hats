5c3addbf70c875b35e18e17f3ff8637c187f5d2b
anamika65 <anamika.buet@gmail.com>
2014-01-23 00:58:02 +0100

    User friendly front page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,9 +10,8 @@ $.fn.scrollView = function() {
 };
 
 $(function() {
-
+	
 	filepicker.setKey("ALJ5oSFlR428EQekrItRgz");
-
 	$('#addFile').click(function() {
 		filepicker.pick({
 			mimetypes: ['image/*', 'text/plain'],
@@ -116,6 +115,40 @@ $(function() {
 	$('#help-button').click(function() {
 		$('body').chardinJs('start');
 	});
+	var $validator = $("#session-form").validate({
+        rules: {
+        	topicfield: {
+        		required: true,
+        		minlength: 3
+        	}
+        }
+      });
+	$('#rootwizard').bootstrapWizard({
+		onTabShow: function(tab, navigation, index) {
+		var $total = navigation.find('li').length;
+		var $current = index+1;
+		var $percent = ($current/$total) * 100;
+		$('#rootwizard').find('.bar').css({width:$percent+'%'});
+		
+		// If it's the last tab then hide the last button and show the finish instead
+		if($current >= $total) {
+			$('#rootwizard').find('.pager .next').hide();
+			$('#rootwizard').find('.pager .finish').show();
+			$('#rootwizard').find('.pager .finish').removeClass('disabled');
+		} else {
+			$('#rootwizard').find('.pager .next').show();
+			$('#rootwizard').find('.pager .finish').hide();
+		}			
+	},
+	onNext: function(tab, navigation, index) {
+        var $valid = $("#session-form").valid();
+        if(!$valid) {
+                $validator.focusInvalid();
+                return false;
+        }
+	}
+	});
+		
 
 });
 
