37f3eb65b60178309707b6437b4de4eaa4b6c309
anamika65 <anamika.buet@gmail.com>
2014-01-25 18:31:51 +0100

    Dashboard template added

--- app/controllers/Dashboard.scala
+++ app/controllers/Dashboard.scala
@@ -18,9 +18,10 @@ object Dashboard extends Controller {
   /**
    * show summary report
    */
-  def showReport(id: Long) = Action { implicit request =>
-    Logger.debug("Dashboard")
-    Ok(views.html.dashboard("hello"))
+  def showReport() = TODO
+  //Action { implicit request =>
+  //Logger.debug("Dashboard")
+  // Ok(views.html.dashboard("hello"))
 
-  }
+  //}
 }
