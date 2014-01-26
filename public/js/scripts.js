2cb2f9a61ba04fcade8045a252671acf923d290f
Dominique Busser <dombusser@gmail.com>
2014-01-26 12:26:07 +0100

    more fixes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -111,35 +111,35 @@ $(function() {
 			}
 		}
 	});
-	$('#rootwizard').bootstrapWizard(
-			{
-				onTabShow : function(tab, navigation, index) {
-					var $total = navigation.find('li').length;
-					var $current = index + 1;
-					var $percent = ($current / $total) * 100;
-					$('#rootwizard').find('.bar').css({
-						width : $percent + '%'
-					});
-
-					// If it's the last tab then hide the last button and show the finish instead
-					if ($current >= $total) {
-						$('#rootwizard').find('.pager .next').hide();
-						$('#rootwizard').find('.pager .finish').show();
-						$('#rootwizard').find('.pager .finish').removeClass(
-								'disabled');
-					} else {
-						$('#rootwizard').find('.pager .next').show();
-						$('#rootwizard').find('.pager .finish').hide();
-					}
-				},
-				onNext : function(tab, navigation, index) {
-					var $valid = $("#session-form").valid();
-					if (!$valid) {
-						$validator.focusInvalid();
-						return false;
-					}
-				}
+	$('#rootwizard').bootstrapWizard({
+		onTabShow : function(tab, navigation, index) {
+			var $total = navigation.find('li').length;
+			var $current = index + 1;
+			var $percent = ($current / $total) * 100;
+			$('#rootwizard').find('.bar').css({
+				width : $percent + '%'
 			});
+
+			// If it's the last tab then hide the last button and show the finish instead
+			if ($current >= $total) {
+				$('#rootwizard').find('.pager .next').hide();
+				$('#rootwizard').find('.pager .finish').show();
+				$('#rootwizard').find('.pager .finish').removeClass(
+						'disabled');
+			} else {
+				$('#rootwizard').find('.pager .next').show();
+				$('#rootwizard').find('.pager .finish').hide();
+			}
+		},
+		onNext : function(tab, navigation, index) {
+			var $valid = $("#session-form").valid();
+			if (!$valid) {
+				$validator.focusInvalid();
+				return false;
+			}
+		}
+	});
+	
 	$('#dashboardModal').on('shown.bs.modal', function() {
 		$(this).find('.modal-dialog').css({
 			width : 'auto',
