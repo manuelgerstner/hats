9ae9e27d29a281862f481936d7ca5e010d1470d2
NemoOudeis <fuguido@googlemail.com>
2014-02-01 23:38:41 +0100

    Adds awesome bubble size algorithm #113

--- public/js/ProgressBar.js
+++ public/js/ProgressBar.js
@@ -40,6 +40,23 @@ ProgressBar.prototype.add = function(card) {
  */
 ProgressBar.prototype.render = function() {
 	var self = this;
+	/**
+     * Calculate bubble size using logistic distribution, see https://de.wikipedia.org/wiki/Logistische_Verteilung
+     * to play with the parameters see plots at
+     * http://fooplot.com/#W3sidHlwZSI6MCwiZXEiOiIzKzIwLygxK2V4cCgteC81KzIpKSIsImNvbG9yIjoiIzAwMDAwMCJ9LHsidHlwZSI6MTAwMCwid2luZG93IjpbIi05LjEzNzI3NTE1OTk5OTk3IiwiMzQuNDgzNDg2NDM5OTk5OTYiLCItMC42ODgzODQ2NTM4ODI4MTAyIiwiMjYuMTU1MTYwOTQ2MTE3MTY3Il19XQ--
+     **/
+    var getSize = function(count) {
+        /**
+         * stretch changes how many contributions are needed for full size
+         * stretch = 1 -> ~ 10 cards yield full size
+         * stretch = 2 -> ~ 20 cards yield full size
+         * etc
+         **/
+        var stretch = 2;
+        var fullSize = 20; // initial size is about 5
+        // initial size for 0 cards
+        return (3 + (fullSize - 3) / (1 + Math.exp((-count / stretch) + 2)));
+    }
 	for (var hat in self.bubbles) {
 		var count = self.bubbles[hat];
 		//console.log("hat: %s", hat);
@@ -47,7 +64,7 @@ ProgressBar.prototype.render = function() {
 		if (count > 0) {
 			var circle = $(self.container).find('circle.' + hat);
 			//console.log("current size %d", circle.attr("r"));
-			circle.attr('r', (5 + count * self.factor));
+			circle.attr('r', (getSize(count)));
 		}
 	}
 };
