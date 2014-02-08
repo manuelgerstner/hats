dbbc2a5dfbbc99fcc3dc34fb115f405d3dd0a7e4
NemoOudeis <fuguido@googlemail.com>
2014-02-08 12:02:22 +0100

    clean up some broken and redundant code

--- app/models/SessionConfig.scala
+++ app/models/SessionConfig.scala
@@ -4,18 +4,6 @@ import models._
 
 case class SessionConfig(
   topic: String,
-  whiteTimeLimit: Option[Int],
-  whiteAloneTime: Option[Int],
-  yellowTimeLimit: Option[Int],
-  yellowAloneTime: Option[Int],
-  redTimeLimit: Option[Int],
-  redAloneTime: Option[Int],
-  greenTimeLimit: Option[Int],
-  greenAloneTime: Option[Int],
-  blueTimeLimit: Option[Int],
-  blueAloneTime: Option[Int],
-  blackTimeLimit: Option[Int],
-  blackAloneTime: Option[Int],
   adminMailAddress: Option[String],
   mailAddresses: String) {
 
@@ -24,12 +12,6 @@ case class SessionConfig(
     case None    => -1
   }
 
-  def white(sessionId: Long): HatFlow = HatFlow(0, sessionId, 1, get(whiteTimeLimit), get(whiteAloneTime));
-  def red(sessionId: Long): HatFlow = HatFlow(1, sessionId, 2, get(redTimeLimit), get(redAloneTime));
-  def yellow(sessionId: Long): HatFlow = HatFlow(2, sessionId, 3, get(yellowTimeLimit), get(yellowAloneTime));
-  def black(sessionId: Long): HatFlow = HatFlow(3, sessionId, 4, get(blackTimeLimit), get(blackAloneTime));
-  def green(sessionId: Long): HatFlow = HatFlow(4, sessionId, 5, get(greenTimeLimit), get(greenAloneTime));
-  def blue(sessionId: Long): HatFlow = HatFlow(5, sessionId, 6, get(blueTimeLimit), get(blueAloneTime));
   def mailAddressList: List[String] = mailAddresses.split(",").toList.filter(_ != "");
   def adminMail: Option[String] = adminMailAddress match {
     case Some(mail) =>
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,29 +1,8 @@
-var clicked = false;
-var sec = 0;
-var elapsedTime = 0;
-function startClock() {
-	if (clicked === false) {
-		clock = setInterval(stopWatch(), 1000);
-		clicked = true;
-	} else if (clicked === true) {
-	}
-}
-function stopWatch() {
-	sec++;
-}
-function stopClock() {
-	window.clearInterval(clock);
-	sec = 0;
-	clicked = false;
-	startClock();
-}
-
-
 // Scrolls the view down to the config pane on the frontpage
 $.fn.scrollView = function() {
 	return this.each(function() {
 		$('html, body').animate({
-			scrollTop : $(this).offset().top - 10
+			scrollTop: $(this).offset().top - 10
 		}, 1000);
 	});
 };
@@ -33,10 +12,11 @@ $(function() {
 	filepicker.setKey("ALJ5oSFlR428EQekrItRgz");
 	$('#addFile').click(function() {
 		filepicker.pick({
-			mimetypes : [ 'image/*', 'text/plain' ],
-			services : [ 'COMPUTER', 'FACEBOOK', 'GMAIL' ],
+			mimetypes: ['image/*', 'text/plain'],
+			services: ['COMPUTER', 'FACEBOOK', 'GMAIL'],
 		}, function(inkBlob) {
-			var imgUrl = inkBlob.url, imgMine = inkBlob.mime;
+			var imgUrl = inkBlob.url,
+				imgMine = inkBlob.mime;
 			// inject into hidden fields
 			$('#imgUrl').val(imgUrl);
 			$('#imgMime').val(imgMime);
@@ -44,14 +24,14 @@ $(function() {
 	});
 
 	$("table").tablesorter({
-		debug : true
+		debug: true
 	});
 
 	$('#modal-button').click(function() {
 		/* new user? */
 		var isNewUser = ($('#form-user').val() === "New User");
 
-		console.log("is new user? " + isNewUser);
+		console.log("Is new user? = " + isNewUser);
 
 		/* force new user to enter name */
 		if (isNewUser) {
@@ -86,139 +66,77 @@ $(function() {
 			addCard(this);
 		});
 	}
-
-	/*$('#card-form').ajaxForm({
-	        dataType : "json",
-	        type : "post",
-	        //beforeSubmit: showRequest,
-	        success : function(card) {
-	                return;
-	                // if (card.error === true) {
-	                //         alert(card.message);
-	                //         return;
-	                // }
-	                // addCard(card); // post-submit callback
-	                
-	        }
-	});*/
-
 	$('.tooltipster').tooltipster();
 
 	$('.tokenfield').tokenfield();
-
-	// get dashboard for session id
-	$('#indicate-finish').click(
-			function() {
-				window.location.href = '/' + SESSION_ID + '/dashboard';
-				$('body').removeClass().addClass("dashboard");
-				elapsedTime = sec;
-				stopClock();//Stop clock
-				jsRoutes.controllers.Dashboard.saveDuration(SESSION_ID, HAT,
-						elapsedTime).ajax({
-					dataType : "json",
-					type : "post",
-					success : function(data) {
-						if (data.error === true) {
-							alert(data.message);
-						}
-						return;
-					}
-				});
-			});
-
 });
+
 // get websocket up and running
+
 function instantiateSocket() {
 	// connect to WAMPlay server
 	console.log("Connecting to WAMPlay server...");
-	startClock();
 	// successful setup
 	ab.connect(WSURI, function(session) {
 
-		// click handler for add card
-		$("#btnAddCard").click(function() {
-			var newCard = {
-				"thinkingSession" : SESSION_ID,
-				"hat" : $("#form-hat").val(),
-				"content" : $("#content").val(),
-				"username" : $("#form-user").val(),
-				"userId" : USER_ID
-			};
-			var addCardEvent = {
-				"eventType" : "addCard",
-				"eventData" : newCard
-			}
-			var message = JSON.stringify(addCardEvent);
-			session.call(CALL_URI + "#addCard", message)
-		});
+			// click handler for add card
+			$("#btnAddCard").click(function() {
+				var newCard = {
+					"thinkingSession": SESSION_ID,
+					"hat": $("#form-hat").val(),
+					"content": $("#content").val(),
+					"username": $("#form-user").val(),
+					"userId": USER_ID
+				};
+				var addCardEvent = {
+					"eventType": "addCard",
+					"eventData": newCard
+				}
+				var message = JSON.stringify(addCardEvent);
+				session.call(CALL_URI + "#addCard", message)
+			});
 
-		$('#indicate-ready').click(
+			$('#indicate-ready').click(
 				function() {
 					var hatInfo = {
-						"thinkingSession" : SESSION_ID,
-						"hat" : $("#form-hat").val()
+						"thinkingSession": SESSION_ID,
+						"hat": $("#form-hat").val()
 					};
 					var moveHatEvent = {
-						"eventType" : "moveHat",
-						"eventData" : hatInfo
+						"eventType": "moveHat",
+						"eventData": hatInfo
 					}
 					var message = JSON.stringify(moveHatEvent);
 					session.call(CALL_URI + "#moveHat", message)
-					// jsRoutes.controllers.ThinkingSessions.restChangeHat(SESSION_ID).ajax({
-					//         dataType : "json",
-					//         type : "post",
-					//         success : function(data) {
-					//                 if (data.error === true) {
-					//                         alert(data.message);
-					//                         return;
-					//                 }
-					//                 $('circle.' + data.hat).show();
-					//                 moveTo(data.hat);
-					//         }
-					// });
-					elapsedTime = sec;
-					stopClock();//Stop clock
-					jsRoutes.controllers.Dashboard.saveDuration(SESSION_ID,
-							HAT, elapsedTime).ajax({
-						dataType : "json",
-						type : "post",
-						success : function(data) {
-							if (data.error === true) {
-								alert(data.message);
-							}
-							return;
-						}
-					});
 				});
-		console.log("Connected to " + WSURI);
-		// subscribe to add cards here, give a callback
-		// ID needs to be string
-		session.subscribe(SESSION_ID.toString(), onEvent);
-		console.log("Subscribed to session number " + SESSION_ID);
-	},
+			console.log("Connected to " + WSURI);
+			// subscribe to add cards here, give a callback
+			// ID needs to be string
+			session.subscribe(SESSION_ID.toString(), onEvent);
+			console.log("Subscribed to session number " + SESSION_ID);
+		},
 
-	// WAMP session is gone
-	function(code, reason) {
-		console.log("Connection lost (" + reason + ")", true);
-		// should probably reconnect here
-	},
-	// additional options
-	{
-		skipSubprotocolCheck : true,
-		skipSubprotocolAnnounce : true
-	}); // Important! Play rejects all subprotocols for some reason...
+		// WAMP session is gone
+
+		function(code, reason) {
+			console.log("Connection lost (" + reason + ")", true);
+			// should probably reconnect here
+		},
+		// additional options
+		{
+			skipSubprotocolCheck: true,
+			skipSubprotocolAnnounce: true
+		}); // Important! Play rejects all subprotocols for some reason...
 }
 
 // debugging handler for websocket events coming in
+
 function onEvent(topic, event) {
 
 	// add switch case for topic here:
-
 	console.log("Message from topic: " + topic + ":");
 	// event holds the actual message that is being sent
 	console.log(event);
-	// event.username = "FooUser";
-	// event.id = 1e4;
 	//if (userid != incoming user) OR use skip paramters in session.send
 	if (event.eventType === "addCard") {
 		addCard(event.eventData, true);
@@ -231,9 +149,9 @@ function onEvent(topic, event) {
 
 function makeDraggable() {
 	$('#cards-list div.card').draggable({
-		containment : "#cards-list",
-		cursor : "move",
-		stack : '.draggable'
+		containment: "#cards-list",
+		cursor: "move",
+		stack: '.draggable'
 	});
 }
 
@@ -269,12 +187,10 @@ function addCard(card, effect) {
 	 * card json: {"id":5, "hat": "Green", "content": "card content",
 	 * "username":"username"}
 	 */
-
 	var template = Handlebars.compile($('#card-template').html());
 	var compiled = template(card);
 
 	$('#cards-list').append(compiled);
-
 	//if (effect) markup.effect('highlight', {}, 1000);
 
 	// reset card content field
@@ -283,5 +199,4 @@ function addCard(card, effect) {
 
 	window.progressBar.add(card);
 	makeDraggable();
-
 }
