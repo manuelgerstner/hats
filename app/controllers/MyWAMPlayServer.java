ac4fb826e41de0432169c7e2cc395d8a2dd4f528
Dominique Busser <dombusser@gmail.com>
2014-02-10 09:23:26 +0100

    ws dependency

--- app/controllers/MyWAMPlayServer.java
+++ app/controllers/MyWAMPlayServer.java
@@ -1,11 +1,15 @@
 package controllers;
 
+import play.mvc.WebSocket;
 import ws.wamplay.controllers.WAMPlayServer;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 public class MyWAMPlayServer extends WAMPlayServer {
 
-	public MyWAMPlayServer() {
-		super();
+	public static WebSocket<JsonNode> connect() {
+		WebSocket<JsonNode> ws = WAMPlayServer.connect();
+		return ws;
 	}
 
 }
