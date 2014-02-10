a61cf9e3592ad2c530d0195c5378dba346c33564
Dominique Busser <dombusser@gmail.com>
2014-02-10 10:37:56 +0100

    Revert "ws dependency"
    This reverts commit ac4fb826e41de0432169c7e2cc395d8a2dd4f528.

--- app/controllers/MyWAMPlayServer.java
+++ app/controllers/MyWAMPlayServer.java
@@ -1,15 +1,11 @@
 package controllers;
 
-import play.mvc.WebSocket;
 import ws.wamplay.controllers.WAMPlayServer;
-
-import com.fasterxml.jackson.databind.JsonNode;
 
 public class MyWAMPlayServer extends WAMPlayServer {
 
-	public static WebSocket<JsonNode> connect() {
-		WebSocket<JsonNode> ws = WAMPlayServer.connect();
-		return ws;
+	public MyWAMPlayServer() {
+		super();
 	}
 
 }
