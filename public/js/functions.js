7af212325ea8ed74b9c0a1144cff456ccc9bc90d
Dominique Busser <dombusser@gmail.com>
2014-02-16 22:03:51 +0100

    bugfixes donut chart, dashboard redesign

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,16 +1,31 @@
 function timeSince(date) {
-    var seconds = Math.floor((new Date() - date) / 1000);
-    var interval = Math.floor(seconds / 31536000);
-    interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-        return interval + " hours";
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) {
-        return interval + " minutes";
-    } else {
-    	return "less than a minute"
-    }
+	var seconds = Math.floor((new Date() - date) / 1000);
+	var interval = Math.floor(seconds / 31536000);
+	interval = Math.floor(seconds / 3600);
+	if (interval > 1) {
+		return interval + " hours";
+	}
+	interval = Math.floor(seconds / 60);
+	if (interval > 1) {
+		return interval + " minutes";
+	} else {
+		return "less than a minute"
+	}
+}
+
+function formatTime(secs) {
+
+	var sec_num = parseInt(secs, 10);
+	var hours   = Math.floor(sec_num / 3600);
+	var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+	var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+	var str = [];
+	if (seconds > 0) str.push(seconds + "s");
+	if (minutes > 0) str.push(minutes + "m");
+	if (hours > 0) str.push(hours + "h");
+
+	return str.reverse().join(" ");
 }
 
 function linkify(text) {
